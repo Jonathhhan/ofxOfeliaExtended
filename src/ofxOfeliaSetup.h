4c210847f49d316bd3053cf0298df19d793e208a
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2025-02-27 00:02:58 +0100

    Add files via upload

--- src/ofxOfeliaSetup.h
+++ src/ofxOfeliaSetup.h
@@ -1,7 +1,19 @@
 #pragma once
-#include "ofxOfeliaThread.h"
 
-ofxOfeliaThread thread;
+/* ofelia compiled version info */
+#define OFELIA_MAJOR_VERSION 4
+#define OFELIA_MINOR_VERSION 1
+#define OFELIA_BUGFIX_VERSION 0
+
+/* minimum required pd version info */
+#define PD_MAJOR_VERSION_REQUIRED 0
+#define PD_MINOR_VERSION_REQUIRED 49
+
+extern int ofeliaVersionMajor;
+extern int ofeliaVersionMinor;
+extern int ofeliaVersionBugFix;
+
+#include "m_pd.h"
 
 extern "C"
 {
